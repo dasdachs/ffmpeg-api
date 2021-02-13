e9e3a10c4122fd03d9bfb683d4b3b0d05031ad52
Jani Šumak <jani.sumak@inova.si>
2021-02-13 21:14:08 +0100

    WIP: file upload handler

--- server.go
+++ server.go
@@ -6,10 +6,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 const PORT = "8080"
+const MAX_UPLOAD_SIZE = 10 * 1024 * 1024 // 2 MB
 
 func ReadEnvFile() []string {
 	_, err := os.Stat(".env")
@@ -52,17 +54,54 @@ func main() {
 
 	// Setup simple logger
 	log.SetFlags(2 | 3)
-	log.SetPrefix("[FFMPEG servie] ")
+	log.SetPrefix("[FFMPEG service] ")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			log.Printf("POST request")
+		if r.Method == http.MethodPost {
+			if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+				log.Printf("Could not parse multipart form: %v\n", err)
+				http.Error(w, "Could not process data", http.StatusInternalServerError)
+			}
 
-			r.ParseForm()
+			file, fileHeader, err := r.FormFile("file")
+			if err != nil {
+				http.Error(w, "File not correct", http.StatusBadRequest)
+				return
+			}
 
-			fmt.Fprintf(w, "Got the post request", r.Body)
+			defer file.Close()
+
+			fileSize := fileHeader.Size
+
+			log.Printf("File size (bytes): %v\n", fileSize)
+
+			if fileSize > MAX_UPLOAD_SIZE {
+				http.Error(w, "FILE_TOO_BIG", http.StatusBadRequest)
+				return
+			}
+
+			fileBytes, err := ioutil.ReadAll(file)
+			if err != nil {
+				http.Error(w, "INVALID_FILE", http.StatusBadRequest)
+			}
+			newPath := filepath.Join("temp", fileHeader.Filename)
+			log.Printf(newPath)
+
+			newFile, err := os.Create(newPath)
+			if err != nil {
+				http.Error(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+				return
+			}
+			defer newFile.Close()
+			if _, err := newFile.Write(fileBytes); err != nil {
+				http.Error(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+				return
+			}
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte("File saved to disk"))
+
 		} else {
-			fmt.Fprintf(w, "Only post methods allowed")
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		}
 	})
 
